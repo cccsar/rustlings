150919e5ae701334c9e2cf9d3b283e1fabaab449
cesar <15-11295@usbve>
2023-03-27 11:44:44 -0400

    Finished 'strings' section. Probably the hardest one

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,54 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut output = String::new();
+    let mut itx = input.as_bytes().into_iter();
+
+    // Disregard heading whitespace with iterator
+    loop {
+        match itx.next() {
+            std::option::Option::Some(chr) => {
+                if chr.to_owned() != b' ' {
+                    output.push(  chr.to_owned() as char );
+                    break;
+                }
+
+            }
+            _ => break,
+        }
+    }
+
+    // Construct remaining string
+    loop {
+        match itx.next() {
+            std::option::Option::Some(chr) => 
+            {
+                output.push( chr.to_owned() as char );
+            }
+            _ => break,
+        }
+    }
+
+    while output.len() > 0 && output.as_bytes()[ output.len() - 1 ] == b' ' {
+        output.pop();
+    }
+
+    output
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut output = String::from(input);
+    output.push_str(" world!");
+    output
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut output = String::from(input);
+    output.replace("car","balloon")
 }
 
 #[cfg(test)]
